5870af8bba1ca3fe632db07b8a2a715d13dc1fa8
Ilann Cote <cote.ilann@live.ca>
2021-10-20 13:50:36 -0400

    first commit chain project

---
+++ chain/chain.lua
@@ -0,0 +1,58 @@
+--creates a table describing the links of a chain
+--the chain will lie on the x axis starting from the origin
+function new_chain(length, nodes, stretch_factor)
+  local chain = {}
+  chain.n = nodes
+  chain.k = stretch_factor
+  local nodedistance = (length/nodes)
+  chain.nodedistance = nodedistance
+  for i = 1, nodes do
+    table.insert(chain, {nodedistance*(i-1), 0, 0, 0}) --{x, y, vx, vy}
+  end
+  return chain
+end
+
+local function sign(number)
+   if number > 0 then
+      return 1
+   elseif number < 0 then
+      return -1
+   else
+      return 0
+   end
+end
+
+--does a simple newton method iteration of the chain
+function iterate(chain, dt)
+  local xforces = {}
+  local yforces = {}
+
+  local dx = {}
+  local dy = {}
+
+  for i = 1, chain.n-1 do
+    dx[i+0.5] = chain[i][1] - chain[i+1][1]
+    dy[i+0.5] = chain[i][2] - chain[i+1][2]
+  end
+
+  local x1, x2
+  for i = 2, chain.n-1 do
+    x1 = sign(dx[i-0.5])*(math.abs(dx[i-0.5]) - chain.nodedistance)
+    x2 = -sign(dx[i+0.5])*(math.abs(dx[i+0.5]) - chain.nodedistance)
+    xforces[i] = chain.k*(x1+x2)
+
+    y1 = sign(dy[i-0.5])*(math.abs(dy[i-0.5]) - chain.nodedistance)
+    y2 = -sign(dy[i+0.5])*(math.abs(dy[i+0.5]) - chain.nodedistance)
+    yforces[i] = chain.k*(y1+y2)
+  end
+
+  for i = 2, chain.n-1 do
+    --update velocity
+    chain[i][3] = chain[i][3] + xforces[i] * dt
+    chain[i][4] = chain[i][4] + yforces[i] * dt
+
+    --update position
+    chain[i][1] = chain[i][1] + chain[i][3]
+    chain[i][2] = chain[i][2] + chain[i][4]
+  end
+end 
---
+++ chain/main.lua
@@ -0,0 +1,45 @@
+
+function love.load()
+
+  width, height = love.graphics.getDimensions( )
+
+
+
+
+end
+
+function love.update(dt)
+
+
+
+
+
+end
+
+
+function love.draw()
+
+
+
+
+end
+
+function love.mousepressed( x, y, button, istouch, presses )
+
+
+
+end
+
+function love.mousemoved( x, y, dx, dy, istouch )
+
+end
+
+function love.keypressed( key, scancode, isrepeat )
+
+
+
+end
+
+function love.wheelmoved(x, y)
+
+end
